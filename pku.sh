2bf6b523a3bdbd2bbd9bc2349a0ed9dfb2e529bd
mattachu <gitkraken@matteaston.net>
2017-12-13 09:24:27 +0800

    Added section for EPOCH

--- pku.sh
+++ pku.sh
@@ -2,14 +2,17 @@
 
 alias server="ssh wangzhi@162.105.147.95"
 alias transfer="rsync --archive --update --delete --max-size=50M --verbose ~/Simulations/Current/ ~/Simulations/Transfer/"
+alias run_tests='./run_tests.sh'
 
+# Shortcuts for Impact-T
 alias impact="run_with_log impact"
 alias impact-test="run_with_log impact-test"
 alias impact-multibunch="ln -s ImpactT.in ImpactT.txt; run_with_log impact-multibunch"
 alias impact-mb="ln -s ImpactT.in ImpactT.txt; run_with_log impact-multibunch"
 alias impact-pku="ln -s ImpactT.in ImpactT.txt; run_with_log impact-pku"
 alias impact-official="run_with_log impact-official"
-
 alias rm-impact="rm ./fort.* ./*.log ./*.dst ./*.plt ./*.csv"
 
-alias run_tests='./run_tests.sh'
+# Settings for EPOCH
+export COMPILER=gfortran
+export MPIF90=mpifort
