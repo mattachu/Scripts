124cccd1cae2d3b9c00737cb2789e275e0bf4f7a
mattachu <gitkraken@matteaston.net>
2017-12-20 09:39:17 +0800

    Simplify PKU versions of Impact-T

--- pku.sh
+++ pku.sh
@@ -7,9 +7,7 @@ alias run_tests='./run_tests.sh'
 # Shortcuts for Impact-T
 alias impact="run_with_log impact"
 alias impact-test="run_with_log impact-test"
-alias impact-multibunch="ln -s ImpactT.in ImpactT.txt; run_with_log impact-multibunch"
-alias impact-mb="ln -s ImpactT.in ImpactT.txt; run_with_log impact-multibunch"
-alias impact-pku="ln -s ImpactT.in ImpactT.txt; run_with_log impact-pku"
+alias impact-pku="run_with_log impact-pku"
 alias impact-official="run_with_log impact-official"
 alias rm-impact="rm ./fort.* ./*.log ./*.dst ./*.plt ./*.csv"
 
