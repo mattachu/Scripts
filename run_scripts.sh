0935216f76b2bec04016ea0aad0d353682c6d67f
mattachu <gitkraken@matteaston.net>
2018-06-12 10:41:25 +0800

    Add output of current command when logging

--- run_scripts.sh
+++ run_scripts.sh
@@ -23,6 +23,7 @@ function run_with_timer {
 function run_with_log {
     rm -f $1.log
     echo "Current directory: " $(pwd -P) > >(tee -a $1.log) 2> >(tee -a $1.log >&2)
+    echo "Current command: $*" > >(tee -a $1.log) 2> >(tee -a $1.log >&2)
     echo "Current time: " $(date) > >(tee -a $1.log) 2> >(tee -a $1.log >&2)
     SECONDS=0
     $* > >(tee -a $1.log) 2> >(tee -a $1.log >&2)
