1cb4ad715807394f41dd0bc4c2400f22316e4df3
mattachu <gitkraken@matteaston.net>
2018-08-24 16:38:56 +0800

    Changed encoding and line endings for tmate.sh
    Was having some problems sourcing the script

--- tmate.sh
+++ tmate.sh
@@ -17,17 +17,17 @@ TMATE_TMUX_SESSION="/tmp/tmate-tmux-session"        # default TMUX location
 # If a session name is given as an argument, it looks for this session,
 #   otherwise the main session is used.
 tmate-url() {
-if [ -n "$1" ]; then
-TMATE_SESSION="$1"
-TMATE_SOCKET="/tmp/tmate-$1.sock"
-else
-TMATE_SESSION="$TMATE_MAIN_SESSION"
-TMATE_SOCKET="$TMATE_MAIN_SOCKET"
-fi
-url="$(tmate -S $TMATE_SOCKET display -p '#{tmate_ssh}')"
-echo "$url" | tr -d '\n' | pbcopy
-echo "Copied tmate url for $TMATE_SESSION session: "
-echo "$url"
+    if [ -n "$1" ]; then
+        TMATE_SESSION="$1"
+        TMATE_SOCKET="/tmp/tmate-$1.sock"
+    else
+        TMATE_SESSION="$TMATE_MAIN_SESSION"
+        TMATE_SOCKET="$TMATE_MAIN_SOCKET"
+    fi
+    url="$(tmate -S $TMATE_SOCKET display -p '#{tmate_ssh}')"
+    echo "$url" | tr -d '\n' | pbcopy
+    echo "Copied tmate url for $TMATE_SESSION session: "
+    echo "$url"
 }
 
 # Start a new tmate pair session if one doesn't already exist
@@ -35,48 +35,48 @@ echo "$url"
 #   1. TMUX session to connect to
 #   2. Session name for tmate
 tmate-pair() {
-if [ -n "$2" ]; then
-TMATE_SESSION="$2"
-TMATE_SOCKET="/tmp/tmate-$2.sock"
-else
-TMATE_SESSION="$TMATE_MAIN_SESSION"
-TMATE_SOCKET="$TMATE_MAIN_SOCKET"
-fi
+    if [ -n "$2" ]; then
+        TMATE_SESSION="$2"
+        TMATE_SOCKET="/tmp/tmate-$2.sock"
+    else
+        TMATE_SESSION="$TMATE_MAIN_SESSION"
+        TMATE_SOCKET="$TMATE_MAIN_SOCKET"
+    fi
 
-if [ ! -e "$TMATE_SOCKET" ]; then
+    if [ ! -e "$TMATE_SOCKET" ]; then
 
-# Start a new tmate session
-tmate -S "$TMATE_SOCKET" -f "$HOME/.tmate.conf" new-session -d -s "$TMATE_SESSION"
+        # Start a new tmate session
+        tmate -S "$TMATE_SOCKET" -f "$HOME/.tmate.conf" new-session -d -s "$TMATE_SESSION"
 
-# Get url
-while [ -z "$url" ]; do
-url="$(tmate -S $TMATE_SOCKET display -p '#{tmate_ssh}')"
-done
-tmate-url
-sleep 1
+        # Get url
+        while [ -z "$url" ]; do
+            url="$(tmate -S $TMATE_SOCKET display -p '#{tmate_ssh}')"
+        done
+        tmate-url
+        sleep 1
 
-# Connect to existing TMUX session
-if [ -n "$1" ]; then
-echo $1 > $TMATE_TMUX_SESSION
-tmate -S "$TMATE_SOCKET" send -t "$TMATE_SESSION" "TMUX='' tmux attach-session -t $1" ENTER
-fi
-fi
+        # Connect to existing TMUX session
+        if [ -n "$1" ]; then
+            echo $1 > $TMATE_TMUX_SESSION
+            tmate -S "$TMATE_SOCKET" send -t "$TMATE_SESSION" "TMUX='' tmux attach-session -t $1" ENTER
+        fi
+    fi
 
-# Connect to existing tmate session
-tmate -S "$TMATE_SOCKET" attach-session -t "$TMATE_SESSION"
+    # Connect to existing tmate session
+    tmate -S "$TMATE_SOCKET" attach-session -t "$TMATE_SESSION"
 }
 
 # Close the pair because security
 tmate-unpair() {
-if [ -e "$TMATE_SOCKET_LOCATION" ]; then
-if [ -e "$TMATE_TMUX_SESSION" ]; then
-tmux detach -s $(cat $TMATE_TMUX_SESSION)
-rm -f $TMATE_TMUX_SESSION
-fi
+    if [ -e "$TMATE_SOCKET_LOCATION" ]; then
+        if [ -e "$TMATE_TMUX_SESSION" ]; then
+            tmux detach -s $(cat $TMATE_TMUX_SESSION)
+            rm -f $TMATE_TMUX_SESSION
+        fi
 
-tmate -S "$TMATE_SOCKET_LOCATION" kill-session -t "$TMATE_PAIR_NAME"
-echo "Killed session $TMATE_PAIR_NAME"
-else
-echo "Session already killed"
-fi
+        tmate -S "$TMATE_SOCKET_LOCATION" kill-session -t "$TMATE_PAIR_NAME"
+        echo "Killed session $TMATE_PAIR_NAME"
+    else
+        echo "Session already killed"
+    fi
 }
