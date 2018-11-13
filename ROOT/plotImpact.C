a3b71b526c105ba328f6508d4d685498c43f4f10
mattachu <gitkraken@matteaston.net>
2018-11-13 14:39:34 +0800

    Script to plot particle count from Impact-T data
    Currently hard-coded for two bunches, and missing all the decoration for the plot.

---
+++ ROOT/plotImpact.C
@@ -0,0 +1,21 @@
+// Plot particle data loaded from `fort.11` (hard-coded to 4 bunches for now)
+void plotImpactParticles(TTree *impact_data){
+
+    // Set canvas properties
+    TCanvas *impact_canvas = new TCanvas("impact_canvas", "Impact-T plots");
+    impact_canvas->SetWindowSize(800, 500);
+
+    // Draw the first dataset (back layer, all four bunches)
+    impact_data->Draw("bunches.n1+bunches.n2+bunches.n3+bunches.n4:bunches.z", "", "");
+    // Draw the second dataset (next layer, without the fourth bunch)
+    impact_data->Draw("bunches.n1+bunches.n2+bunches.n3:bunches.z", "", "same");
+    // Draw the third dataset (next layer, without the third and fourth bunches)
+    impact_data->Draw("bunches.n1+bunches.n2:bunches.z", "", "same");
+    // Draw the fourth dataset (top layer, first bunch only)
+    impact_data->Draw("bunches.n1:bunches.z", "", "same");
+
+    // Update canvas
+    impact_canvas->Update();
+    impact_canvas->Paint();
+
+}
