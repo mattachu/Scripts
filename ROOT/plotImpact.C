02f027e39a9a54b91630848aa77255ab5ea83a22
mattachu <gitkraken@matteaston.net>
2018-11-13 15:57:15 +0800

    Added `renameCurrentGraph` function for _ROOT_

--- ROOT/plotImpact.C
+++ ROOT/plotImpact.C
@@ -1,3 +1,13 @@
+// Function to make the current graph look pretty
+void renameCurrentGraph(TCanvas *impact_canvas, const char *name) {
+
+    // Get the current graph
+    TGraph *current_graph = (TGraph *) impact_canvas->GetPrimitive("Graph");
+
+    // Reset the graph name (to make it easier to find later)
+    current_graph->SetName(name);
+}
+
 // Plot particle data loaded from `fort.11` (hard-coded to 4 bunches for now)
 TCanvas * plotImpactParticles(TTree *impact_data){
 
@@ -7,12 +17,16 @@ TCanvas * plotImpactParticles(TTree *impact_data){
 
     // Draw the first dataset (back layer, all four bunches)
     impact_data->Draw("bunches.n1+bunches.n2+bunches.n3+bunches.n4:bunches.z", "", "");
+    renameCurrentGraph(impact_canvas, "graph4");
     // Draw the second dataset (next layer, without the fourth bunch)
     impact_data->Draw("bunches.n1+bunches.n2+bunches.n3:bunches.z", "", "same");
+    renameCurrentGraph(impact_canvas, "graph3");
     // Draw the third dataset (next layer, without the third and fourth bunches)
     impact_data->Draw("bunches.n1+bunches.n2:bunches.z", "", "same");
+    renameCurrentGraph(impact_canvas, "graph2");
     // Draw the fourth dataset (top layer, first bunch only)
     impact_data->Draw("bunches.n1:bunches.z", "", "same");
+    renameCurrentGraph(impact_canvas, "graph1");
 
     // Update canvas
     impact_canvas->Update();
