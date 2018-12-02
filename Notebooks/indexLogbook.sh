46a9f5c8cc39b1dc4f68e027997def1587e58b20
mattachu <github@matteaston.net>
2018-12-02 22:08:00 +0800

    Updated `indexLogbook.sh` to be compatible with macOS

--- Notebooks/indexLogbook.sh
+++ Notebooks/indexLogbook.sh
@@ -5,12 +5,18 @@
 # Parameters
 datePattern="./[0-9]{4}-[0-9]{2}-[0-9]{2}.md"
 monthPattern="./[0-9]{4}-[0-9]{2}.md"
+if [[ "$(uname)" == "Darwin" ]]; then
+    findCommand="find -E ."
+else
+    findCommand="find . -regextype posix-egrep"
+fi
 
 # Clear out existing versions of monthly summary files
-find . -regextype posix-egrep -regex $monthPattern -delete
+$findCommand -regex $monthPattern -delete
 
 # Get list of files matching date pattern `XXXX-XX-XX.md`
-dateList=$(find . -regextype posix-egrep -regex $datePattern | \
+dateList=$($findCommand -regex $datePattern | \
+           sort | \
            sed -e 's|\./||' -e 's|\.md||')
 
 # Work through list of dates
@@ -26,8 +32,9 @@ do
             # Link to last month
             echo -e "[< $lastMonth]($lastMonth)\n" > "$currentMonth.md"
             # Link to current month in last month page
-            sed -i "1s/\$/ | [$currentMonth >]($currentMonth)/" "$lastMonth.md"
-            sed -i '1s/^ | //' "$lastMonth.md"
+            sed -i "" -e "1s/\$/ | [$currentMonth >]($currentMonth)/" \
+                "$lastMonth.md"
+            sed -i "" -e '1s/^ | //' "$lastMonth.md"
         else
             echo -e "\n" > "$currentMonth.md"
         fi
@@ -35,16 +42,17 @@ do
         lastMonth="$currentMonth"
     fi
     # Add forward, back and up links to logbooks
-    sed -i "1s/^\([^[]\)/\n\n\1/" "$currentDate.md"
+    sed -i "" -e "1s/^\([^[]\)/\
+\1/" "$currentDate.md"
     if [[ -n "$lastDate" ]]; then
-        sed -i "1s/^.*\$/[< $lastDate]($lastDate)/" "$currentDate.md"
+        sed -i "" -e "1s/^.*\$/[< $lastDate]($lastDate)/" "$currentDate.md"
     else
-        sed -i "1s/^.*\$//" "$currentDate.md"
+        sed -i "" -e "1s/^.*\$//" "$currentDate.md"
     fi
-    sed -i "1s/\$/ | [$currentMonth]($currentMonth)/" "$currentDate.md"
+    sed -i "" -e "1s/\$/ | [$currentMonth]($currentMonth)/" "$currentDate.md"
     if [[ -n "$lastDate" ]]; then
-        sed -i "1s/\$/ | [$currentDate >]($currentDate)/" "$lastDate.md"
-        sed -i '1s/^ | //' "$lastDate.md"
+        sed -i "" -e "1s/\$/ | [$currentDate >]($currentDate)/" "$lastDate.md"
+        sed -i "" -e '1s/^ | //' "$lastDate.md"
     fi
     lastDate="$currentDate"
     # Summarise contents:
@@ -54,6 +62,7 @@ do
     # - use grep to find major headings (# and ##)
     # - remove line breaks and add colons
     # - replace headings with bullet points
+    # - `sed` on macOS cannot use '\n' so needs literal newlines
     echo -e "## [$currentDate]($currentDate)" >> "$currentMonth.md"
     if [[ ! -n $(sed -n '3p' "$currentDate.md" | grep -e '^#') ]]; then
         echo >> "$currentMonth.md"
@@ -64,14 +73,20 @@ do
             -e 's/\[\([^]]*\)\]([^)]*)/\1/g' \
             -e 's/^#.*$//' \
             >> "$currentMonth.md"
-        echo >> "$currentMonth.md"
+        if [[ ! "$(uname)" == "Darwin" ]]; then echo >> "$currentMonth.md"; fi
     fi
     sed -e '/Logbook:/d' -e '/^\[.*]:/d' -e '/^$/d' -e 's/`//g' \
         -e 's/\[\([^]]*\)\]\[[^]]*\]/\1/g' -e 's/\[\([^]]*\)\]([^)]*)/\1/g' \
         "$currentDate.md" | \
-    grep -Pzo -e "[^#]#{1,2} [^.:!?]*[.:!?]" | sed -e 's/:/./g' | \
-    sed -e 's/\(# .*\)$/\1: /g' | tr '\n' ' ' | tr -d '\r' | tr -d '\000' | \
-    sed -e 's/## /\n    - /g' -e 's/# /\n* /g' -e 's/ \* / /g' \
+    pcregrep -Mo -e "[^#]#{1,2} [^.:!?]*[.:!?]" | sed -e 's/:/./g' | \
+    sed -e 's/\(# .*\)$/\1: /g' | tr '\n' ' ' | tr -d '\r' | \
+    sed -e 's/## /\
+    - /g' -e 's/# /\
+* /g' -e 's/ \* / /g' \
         >> "$currentMonth.md"
-    echo -e "\n" >> "$currentMonth.md"
+    if [[ "$(uname)" == "Darwin" ]]; then
+        echo >> "$currentMonth.md"
+    else
+        echo -e "\n" >> "$currentMonth.md"
+    fi
 done
