7203b4e62c99d29ed4d6d91693ea6e443e873737
mattachu <github@matteaston.net>
2020-06-11 13:51:49 +0100

    Add code to add and load items

--- process_notebooks.py
+++ process_notebooks.py
@@ -17,10 +17,17 @@ class Page():
         if page_file is not None:
             if page_file.is_file():
                 self.path = page_file
-                with open(page_file, 'r') as f:
-                    self.content = f.readlines()
+                self.load_content()
             else:
                 raise OSError(f'Cannot find file: {page_file}')
+
+    def load_content(self):
+        """Load the content of the page from file."""
+        if self.path is None:
+            self.content = None
+        else:
+            with open(self.path, 'r') as f:
+                self.content = f.readlines()
 
     def get_title(self):
         """Find the title of a page."""
@@ -52,16 +59,60 @@ class LogbookPage(Page):
 class Notebook():
     """Standard notebook object containing pages."""
     def __init__(self, notebook_path=None):
+        self.contents = None
         self.path = None
         if notebook_path is not None:
             if notebook_path.is_dir():
                 self.path = notebook_path
+                self.load_contents()
             else:
                 raise OSError(f'Cannot find path: {notebook_path}')
 
+    def load_contents(self):
+        """Load the contents of the notebook from its directory."""
+        if self.path is None:
+            self.contents = None
+        else:
+            self.contents = []
+            for item in self.path.iterdir():
+                if item.is_file():
+                    self.add_page(item)
+                elif item.is_dir():
+                    self.add_folder(item)
+
+    def add_page(self, page_path=None):
+        """Add a page to a notebook."""
+        if self.contents is None:
+            self.contents = []
+        self.contents.append(Page(page_path))
+
+    def add_folder(self, folder_path):
+        """Add a subfolder to a notebook, either as a notebook or logbook."""
+        if folder_path.stem == 'Logbook':
+            self.add_logbook(folder_path)
+        else:
+            self.add_notebook(folder_path)
+
+    def add_notebook(self, notebook_path=None):
+        """Add a nested notebook inside a notebook."""
+        if self.contents is None:
+            self.contents = []
+        self.contents.append(Notebook(notebook_path))
+
+    def add_logbook(self, logbook_path=None):
+        """Add a nested logbook inside a notebook."""
+        if self.contents is None:
+            self.contents = []
+        self.contents.append(Logbook(logbook_path))
+
 class Logbook(Notebook):
     """Special notebook object for logbooks, containing logbook pages."""
-    pass
+
+    def add_page(self, page_path=None):
+        """Add a page to a logbook."""
+        if self.contents is None:
+            self.contents = []
+        self.contents.append(LogbookPage(page_path))
 
 
 # Utility functions
